25f6a955d89e539b499064dd6817126f484b1d60
Aaron Rowe-Jardine <aaron.rowe@websurgeon.ca>
2025-11-14 14:51:45 -0500

    Initialize bookstore models, controllers, and migrations
    Added basic Eloquent models and controllers for Author, Book, Checkout, and Tag. Created initial database migrations for authors, books, checkouts, tags, and taggables tables to support core bookstore functionality. Included a README with setup instructions.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Isolocity Bookstore
+
+- Setup Laravel / React / Inertia: https://laravel.com/docs/12.x/starter-kits
---
+++ database/migrations/2025_11_14_190107_create_checkouts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('checkouts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('book_id')->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->timestamp('checked_out_at');
+            $table->timestamp('due_at');
+            $table->timestamp('returned_at')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('checkouts');
+    }
+};
